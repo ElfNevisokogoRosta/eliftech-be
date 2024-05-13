b3397891c39cd230a7140278c80fa4160cb09d31
ElfNevisokogoRosta <anatoliy.tymoshenko.work@gmail.com>
2024-05-13 14:00:53 +0300

    init

---
+++ db/entity/user.entity.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column } from 'typeorm/browser';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  first_name: string;
+
+  @Column()
+  last_name: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  email: string;
+}
